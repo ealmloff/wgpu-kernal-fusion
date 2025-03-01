0f13ebc65d07a04674869a34cd804b8fc4a7d1bc
Evan Almloff <evanalmloff@gmail.com>
2025-03-01 17:19:29 -0600

    add a test for narrow

--- src/composite/narrow.rs
+++ src/composite/narrow.rs
@@ -14,3 +14,26 @@ impl<const R: usize, D: DataType> Tensor<R, D> {
         }))
     }
 }
+
+#[cfg(test)]
+#[tokio::test]
+async fn test_narrow() {
+    use crate::Device;
+
+    let device = Device::new().await.unwrap();
+    std::thread::spawn({
+        let device = device.clone();
+        move || loop {
+            device.wgpu_device().poll(wgpu::PollType::Wait).unwrap();
+        }
+    });
+    let data = [[1., 2.], [3., 4.], [5., 6.]];
+    let tensor = Tensor::new(&device, &data);
+    let narrowed = tensor.narrow(0, 1, 2);
+    let as_slice = narrowed.as_slice().await.unwrap();
+    println!("{:?}", as_slice);
+    assert_eq!(as_slice[[0, 0]], 3.);
+    assert_eq!(as_slice[[0, 1]], 4.);
+    assert_eq!(as_slice[[1, 0]], 5.);
+    assert_eq!(as_slice[[1, 1]], 6.);
+}
